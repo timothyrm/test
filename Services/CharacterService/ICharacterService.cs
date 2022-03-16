2ded8b814a006f2167429dbe5f77e13980189998
timothyrm <70263972+timothyrm@users.noreply.github.com>
2022-03-16 14:45:10 +0100

    commit

---
+++ Services/CharacterService/ICharacterService.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using test.Dtos.Character;
+using test.Models;
+
+namespace test.Services.CharacterService
+{
+    public interface ICharacterService
+    {
+         Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters();
+         Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id);
+         Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter);
+         Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto updatedCharacter);
+         Task<ServiceResponse<List<GetCharacterDto>>> DeleteCharacter(int id);
+        Task UpdateCharacter(int id);
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using test.Services.CharacterService;
 
 namespace test
 {
@@ -32,6 +33,8 @@ namespace test
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "test", Version = "v1" });
             });
+            services.AddAutoMapper(typeof(Startup));
+            services.AddScoped<ICharacterService, CharacterService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
